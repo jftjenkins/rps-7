acc1f6daba4436ed5eb41361b5d24880e677f40b
jftjenkins <>
2023-09-18 15:18:17 +0000

    Add java code for rps-7 game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-function runGame() {
+function play(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors', 'water', 'air', 'fire', 'sponge'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    let result = '';
+
+    if (playerChoice === computerChoice) {
+        result = "It's a tie!";
+    } else if (
+        (playerChoice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'fire' || computerChoice === 'sponge')) ||
+        (playerChoice === 'paper' && (computerChoice === 'air' || computerChoice === 'rock' || computerChoice === 'water')) ||
+        (playerChoice === 'scissors' && (computerChoice === 'air' || computerChoice === 'paper' || computerChoice === 'sponge')) ||
+        (playerChoice === 'water' && (computerChoice === 'rock' || computerChoice === 'fire' || computerChoice === 'scissors')) ||
+        (playerChoice === 'air' && (computerChoice === 'fire' || computerChoice === 'rock' || computerChoice === 'water')) ||
+        (playerChoice === 'fire' && (computerChoice === 'scissors' || computerChoice === 'paper' || computerChoice === 'sponge')) ||
+        (playerChoice === 'sponge' && (computerChoice === 'paper' || computerChoice === 'air' || computerChoice === 'water'))
+    ) {
+        alert("You win!");
+        increasePlayerScore();
+    } else {
+        alert("Computer wins!");
+        increaseComputerScore();
+    }
+}
+
+function computerChoice() {
 
 }
 
@@ -23,5 +48,5 @@ function normalMode() {
 }
 
 function hardMode() {
-    
+
 }
