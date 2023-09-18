ee0bb1cd11d8bee3bcd6c9bb993e41f855ce1b60
jftjenkins <>
2023-09-18 10:00:15 +0000

    Add JavaScript layout to script.js file.

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame() {
+
+}
+
+function checkResult() {
+
+}
+
+function increasePlayerScore() {
+
+}
+
+function increaseComputerScore() {
+
+}
+
+function easyMode() {
+
+}
+
+function normalMode() {
+
+}
+
+function hardMode() {
+    
+}
